0277a28bb01088e4c0cf06ec223f5e0a9a181f9f
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2021-10-02 20:46:35 +0200

    Create user and listing entity and create fixture for users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,23 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Book;
+use App\Entity\User;
 use App\Entity\Movie;
 use App\Entity\Music;
+use App\Entity\Listing;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordHasherInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
@@ -57,6 +67,18 @@ class AppFixtures extends Fixture
                 ->setReleasedAt($faker->date('d m Y'));
 
             $manager->persist($book);
+        }
+
+        for ($i = 0; $i < 5; $i++) {
+
+            $user = new User();
+            $user
+                ->setPseudonym($faker->firstName . '.' . $faker->lastName)
+                ->setEmail($faker->email)
+                ->setPassword($this->passwordEncoder->hashPassword($user, 'demo'))
+                ->setList(new Listing);
+
+            $manager->persist($user);
         }
 
         $manager->flush();
---
+++ src/Entity/Listing.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ListingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ListingRepository::class)
+ */
+class Listing
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity=User::class, mappedBy="list", cascade={"persist", "remove"})
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        // set the owning side of the relation if necessary
+        if ($user->getList() !== $this) {
+            $user->setList($this);
+        }
+
+        $this->user = $user;
+
+        return $this;
+    }
+}
